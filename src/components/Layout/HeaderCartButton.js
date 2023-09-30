beb796899a69b216c76c28ac044de2f3b958440a
Pranav Tartey <pranavtartey@gmail.com>
2023-09-30 21:25:40 +0530

    complete working frontend for this app

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
+
+export default function HeaderCartButton(props) {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+    return currNumber + item.amount;
+  }, 0);
+  return (
+    <button onClick={props.onClick} className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+}
